8f94cec414dad194a0d8ef49842ca0d99e74a593
Mks <nisq@ya.ru>
2021-01-22 16:56:59 +0300

    works great with shortCut

--- Shortcut/AppDelegate.swift
+++ Shortcut/AppDelegate.swift
@@ -10,50 +10,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-    var vcsArray = [UIViewController]()
-    
-    
-    
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let mountainVC = storyboard.instantiateViewController(withIdentifier: "mountainVC") as! MountainVC
-        let spaceVC = storyboard.instantiateViewController(withIdentifier: "spaceVC") as! SpaceVC
-        let oceanVC = storyboard.instantiateViewController(withIdentifier: "oceanVC") as! OceanVC
-        
-        vcsArray = [mountainVC, spaceVC, oceanVC]
-        
+      
         return true
-    }
-    
-    enum ShortcutType : String {
-        case mountain = "mountain"
-        case space = "space"
-        case ocean = "ocean"
-    }
-    
-    
-    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
-        //print(shortcutItem.type)
-        if let type = shortcutItem.type.components(separatedBy: ".").last {
-            
-            let navVc = window?.rootViewController as! UINavigationController
-            navVc.setViewControllers(vcsArray, animated: false)
-            
-            switch type {
-            
-            case ShortcutType.space.rawValue:
-                print("This is the space")
-                navVc.popToViewController(vcsArray[1], animated: false)
-            case ShortcutType.ocean.rawValue:
-                print("Love the beach")
-            default:
-                print("These are the mountains")
-            }
-        }
     }
 
     // MARK: UISceneSession Lifecycle
--- Shortcut/SceneDelegate.swift
+++ Shortcut/SceneDelegate.swift
@@ -11,14 +11,56 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    
+    var vcsArray = [UIViewController]()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let mountainVC = storyboard.instantiateViewController(withIdentifier: "mountainVC") as! MountainVC
+        let spaceVC = storyboard.instantiateViewController(withIdentifier: "spaceVC") as! SpaceVC
+        let oceanVC = storyboard.instantiateViewController(withIdentifier: "oceanVC") as! OceanVC
+        
+        vcsArray = [mountainVC, spaceVC, oceanVC]
+       
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
+    // shortcut
+    
+    enum ShortcutType : String {
+        case mountain = "mountain"
+        case space = "space"
+        case ocean = "ocean"
+    }
+    
+    
+    
+    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        //print(shortcutItem.type)
+        if let type = shortcutItem.type.components(separatedBy: ".").last {
+            
+            let navVc = window?.rootViewController as! UINavigationController
+            navVc.setViewControllers(vcsArray, animated: false)
+            
+            switch type {
+            
+            case ShortcutType.space.rawValue:
+                print("This is the space")
+                navVc.popToViewController(vcsArray[1], animated: true)
+                completionHandler(true)
+            case ShortcutType.ocean.rawValue:
+                print("Love the beach")
+                navVc.popToViewController(vcsArray[2], animated: true)
+                completionHandler(true)
+            default:
+                navVc.popToRootViewController(animated: true)
+                print("These are the mountains")
+                completionHandler(true)
+            }
+        }
+        completionHandler(false)
+    }
+//
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
